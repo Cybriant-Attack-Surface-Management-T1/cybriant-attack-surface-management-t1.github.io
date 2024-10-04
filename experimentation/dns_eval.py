498dab6d01f67fd5f39842371bcda6cd3dcde6ff
Twi <mclemoredanard@yahoo.com>
2024-10-04 19:27:51 -0400

    Updated comments on dns_eval
    Included more comments on the file that details parameters and libraries

--- experimentation/dns_eval.py
+++ experimentation/dns_eval.py
@@ -3,21 +3,41 @@ import whois  # python-whois
 import sys  # base
 
 
-def get_domain_info(domain: str) -> [str]:
-    domain_info = []
+def get_domain_info(domain: str) -> {str: str}:
+    """
+    This function finds information about the domain using the whois tool and stores it in a list
+    :param domain: The domain to query
+    :type domain: str
+    :return: Information about the domain
+    :rtype: dict
+    """
+    domain_info = {}
+
+    # try-except statement to look for domain information
     try:
         domain_i = whois.whois(domain)
-        domain_info.append(f'Domain: {domain_i.domain}')
-        domain_info.append(f'Registrar: {domain_i.registrar}')
-        domain_info.append(f'Creation date: {domain_i.creation_date}')
+        domain_info['Domain'] = domain_i.domain  # redundant domain name return
+        domain_info['Registrar'] = domain_i.registrar  # domains registrar
+        domain_info['Creation date'] = domain_i.creation_date  # domains creation date
+        domain_info['Expiration date'] = domain_i.expiration_date  # domains expiration date
+        domain_info['Emails'] = domain_i.emails  # emails attached to domain
+
+    # Bare exception catch
     except Exception as e:
         print(f'Error: {e}')
+        return None
     return domain_info
 
 
-def main(domains: str):
-    domain_info = get_domain_info(domains)
-    print(domain_info)
+def main(domain: str):
+    """
+    Main function that calls the domain information function
+    :param domain: The domain to query
+    :type domain: str
+    """
+    domain_info = get_domain_info(domain)
+    if domain_info:
+        print(domain_info)
 
 
 if __name__ == '__main__':
